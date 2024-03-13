bfc326a2e8e6f142651f7adca54448e87618f987
kamil6708 <73381281+kamil6708@users.noreply.github.com>
2024-03-13 22:10:43 +0300

    Update app.py
    Version 2

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from pytube import YouTube
 import os
+from io import BytesIO
+import base64
 
 def sanitize_filename(filename):
-    # Replace invalid characters with underscores
     invalid_chars = '<>:"/\\|?*'
     for char in invalid_chars:
         filename = filename.replace(char, '_')
@@ -17,27 +18,43 @@ def download_video(link):
 
         yd = yt.streams.get_highest_resolution()
         sanitized_title = sanitize_filename("telechargement_youtube")
-        download_path = os.path.join(os.getcwd(), sanitized_title)
-        st.write("Downloading...")
-        yd.download(download_path)
-        st.write("Download complete! Video saved at:", download_path)
+        download_path = os.path.join(os.getcwd(), sanitized_title + ".mp4")
+
+        # Create a BytesIO object
+        buffer = BytesIO()
+        # Download link for manual download
+        yd.stream_to_buffer(buffer)
+        video_buffer = buffer.getvalue()
+       
+        video_base64 = base64.b64encode(video_buffer).decode()
+        download_link = f'<a href="data:video/mp4;base64,{video_base64}" download="{download_path}">Click here to download</a>'
+        
+        st.markdown("To download the video manually, right-click the link below and choose 'Save link as':", unsafe_allow_html=True)
+        st.markdown(download_link, unsafe_allow_html=True)
+
+        # Progress bar
+        progress_bar = st.progress(0)
+        for i in range(100):
+            # Update the progress bar with each iteration.
+            progress_bar.progress(i + 1)
+    
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
 def main():
-    st.title("Téléchargement video YouTube ")
+    st.title("Téléchargeur de vidéos YouTube")
 
-    # Input for the YouTube video URLS
-    link = st.text_input(" Veuillez mettre le lien URL de la video Youtube: ")
+    # Input for the YouTube video URL
+    link = st.text_input("Veuillez entrer l’URL de la vidéo YouTube:")
 
     if st.button("Download"):
         if link:
             download_video(link)
         else:
-            st.warning("Veuillez mettre un lien youtube valide.")
+            st.warning("Veuillez entrer l’URL de la vidéo YouTube valdie.")
 
     # Copyright notice
-    st.markdown("<p style='text-align: center; color: #888888;'>© Kamil Mohamed Kamil. Tous droits réservés.</p>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; color: #888888;'>© Kamil Mohamed Kamil.Tous droits réservés.</p>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
